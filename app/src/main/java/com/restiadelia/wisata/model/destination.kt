9acf34ff5543141640593cdf65537fe410671c5d
RestiAdelia <adeliaresti101@gmail.com>
2024-09-27 15:35:20 +0700

    update all

--- app/src/main/java/com/restiadelia/wisata/model/destination.kt
+++ app/src/main/java/com/restiadelia/wisata/model/destination.kt
@@ -17,17 +17,55 @@ object MockList {
             descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
         )
         val itemModel2 = destination(
-            imageResId = R.drawable.japan,
+            imageResId = R.drawable.wisata,
             name = "Apple",
             descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
         )
         val itemModel3 = destination(
-            imageResId = R.drawable.japan,
+            imageResId = R.drawable.jepang,
             name = "Apple",
             descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
         )
         val itemModel4 = destination(
+            imageResId = R.drawable.jep,
+            name = "Apple",
+            descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
+        )
+
+
+        val itemList: ArrayList<destination> = ArrayList()
+        itemList.add(itemModel1)
+        itemList.add(itemModel2)
+        itemList.add(itemModel3)
+        itemList.add(itemModel4)
+        return itemList
+
+
+    }
+}
+
+object MockList1 {
+
+    fun getModel(): List<destination> {
+        val itemModel1 = destination(
             imageResId = R.drawable.japan,
+            name = "Gunung fuji",
+            descrip = "Tempat wisata ini menampilkan rumah-rumah adat Jepang yang dikenal sebagai gassho-zukuri, arsitektur tradisional yang unik dengan atap jerami berbentuk segitiga curam, dirancang untuk menahan salju tebal di musim dingin. Desa-desa seperti ini bisa ditemukan di area pedesaan Jepang, seperti Shirakawa-go atau di sekitarnya. Rumah-rumah tersebut dikelilingi oleh suasana alam yang indah, dan dari beberapa titik, pengunjung dapat menikmati pemandangan spektakuler Gunung Fuji yang megah di kejauhan.\n" +
+                    "\n" +
+                    "Pemandangan ini sangat indah terutama saat Gunung Fuji terlihat jelas di musim semi, dengan latar bunga sakura yang bermekaran, atau di musim gugur dengan dedaunan berwarna-warni. Tempat ini tidak hanya menawarkan keindahan arsitektur tradisional Jepang, tetapi juga ketenangan alam yang menggabungkan sejarah dan pesona alam Jepang, menjadikannya destinasi favorit bagi wisatawan yang ingin merasakan keaslian budaya Jepang dengan panorama Gunung Fuji yang ikonik."
+        )
+        val itemModel2 = destination(
+            imageResId = R.drawable.wisata,
+            name = "Tokyo",
+            descrip = "Sebagai ibu kota Jepang, Tokyo adalah pusat ekonomi, politik, dan budaya yang sangat dinamis. Tokyo terkenal dengan gedung pencakar langitnya, distrik bisnis yang padat, dan kawasan hiburan seperti Shibuya, Shinjuku, dan Ginza. Di Tokyo, pengunjung dapat menemukan perpaduan unik antara teknologi canggih dan tradisi kuno, dengan kuil-kuil bersejarah yang berdampingan dengan pusat perbelanjaan modern, seperti Tokyo Tower, Sensoji Temple di Asakusa, dan Tokyo Skytree."
+        )
+        val itemModel3 = destination(
+            imageResId = R.drawable.jepang,
+            name = "Apple",
+            descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
+        )
+        val itemModel4 = destination(
+            imageResId = R.drawable.jep,
             name = "Apple",
             descrip = "dsijdiejdeodkckdciddcdcjdjdkd"
         )
--- app/src/main/java/com/restiadelia/wisata/pageberanda.kt
+++ app/src/main/java/com/restiadelia/wisata/pageberanda.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.restiadelia.wisata.adapter.Adapterwisata
 import com.restiadelia.wisata.model.MockList
+import com.restiadelia.wisata.model.MockList1
 import com.restiadelia.wisata.model.destination
 
 class pageberanda : AppCompatActivity() {
@@ -24,15 +26,14 @@ class pageberanda : AppCompatActivity() {
         rv_wisata = findViewById(R.id.rv_wisata)
         rv_wisata1 = findViewById(R.id.rv_wisata1)
 
-        layoutManager =LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true)
+        rv_wisata.layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
+        rv_wisata1.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.HORIZONTAL, false)
 
-        val adapter = Adapterwisata(MockList.getModel() as ArrayList<destination>,this)
+        val adapter = Adapterwisata(MockList.getModel() as ArrayList<destination>, this)
+        val adapterr = Adapterwisata(MockList1.getModel() as ArrayList<destination>, this)
+
         rv_wisata.adapter = adapter
-
-        // Mengambil data dari Intent
-        val detailtext = intent.getStringExtra("name") ?: "Default Name"
-        val detailgambar = intent.getIntExtra("imageResId", 0)
-        val detaildescripsi = intent.getStringExtra("descrp") ?: "Default Description"
+        rv_wisata1.adapter =adapterr
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
